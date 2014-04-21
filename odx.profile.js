5fbe58f50266f7a639af3843bd2c9529b1c9dd4a
Administrator <Administrator@Addo-PC>
2014-04-22 07:38:20 +0800

    init structure

---
+++ odx.profile.js
@@ -0,0 +1,29 @@
+require([ "dojox/app/build/buildControlApp" ], function(bc) {
+});
+
+var profile = {
+	basePath : "..",
+	releaseDir : "./s/release",
+	action : "release",
+	cssOptimize : "comments",
+	/* multipleAppConfigLayers: true, */
+	packages : [ {
+		name : "dojo",
+		location : "../../../dojo"
+	}, {
+		name : "dijit",
+		location : "../../../dijit"
+	}, {
+		name : "layoutApp",
+		location : "../../../dojox/app/tests/layoutApp",
+		destLocation : "./dojox/app/tests/layoutApp"
+	}, {
+		name : "dojox",
+		location : "../../../dojox"
+	} ],
+	layers : {
+		"layoutApp/layoutApp" : {
+			include : [ "layoutApp/index.html" ]
+		}
+	}
+};
